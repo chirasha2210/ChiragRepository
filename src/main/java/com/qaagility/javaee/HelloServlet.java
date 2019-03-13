90464a43f6134c0445869d9563fb7dc202ff2330
chirasha2210 <chiragdilipoct@gmail.com>
2019-03-13 20:29:32 +0530

    servlet

--- src/main/java/com/qaagility/javaee/HelloServlet.java
+++ src/main/java/com/qaagility/javaee/HelloServlet.java
@@ -31,7 +31,7 @@ public class HelloServlet extends HttpServlet {
 			throws ServletException, IOException {
 		String yourName = request.getParameter("yourName");
 		PrintWriter writer = response.getWriter();
-		writer.println("<h1>Hello " + yourName + "</h1>");
+		writer.println("<h1>SSUUP " + yourName + "</h1>");
 		System.out.println("Done");
 		writer.close();
 	}
